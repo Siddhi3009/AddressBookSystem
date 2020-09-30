f70d2ca880c5d3fa313a18ba93783a49e0fdb970
Administrator <siddhiseth3009@gmail.com>
2020-09-30 15:51:05 +0530

    [Siddhi]Add Contact and AddressBook class added

---
+++ AddressBookSystem/AddressBook.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddressBookSystem
+{
+    class AddressBook
+    {
+        public static List<Contact> People;
+        public AddressBook()
+        {
+            People = new List<Contact>();
+        }
+    }
+}
